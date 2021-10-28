a1c91acc7461800191e8e9be48accce61ae93d3a
c0ldch0c0l8 <c0ldch0c0l8@gmail.com>
2021-10-28 17:29:59 +0300

    more custom widget stuff

--- src/widget.rs
+++ src/widget.rs
@@ -1,16 +1,16 @@
 use iced_graphics::{Backend, Defaults, Renderer, Primitive};
 use iced_native::{Widget, layout, mouse, Element};
-use iced::{Background, Color, Column, Length, Sandbox, Size, Slider, Space, slider};
+use iced::{Background, Color, Column, Length, Sandbox, Size, Slider, Space, slider, Rectangle};
 
 #[derive(Default)]
 pub struct WidgetApp {
-    radius: u16,
+    value: u16,
     slider_state: slider::State
 }
 
 #[derive(Debug, Clone)]
 pub enum Message {
-    RadiusChanged(u16)
+    ValueChanged(u16)
 }
 
 impl Sandbox for WidgetApp {
@@ -26,18 +26,19 @@ impl Sandbox for WidgetApp {
 
     fn view(&mut self) -> iced::Element<'_, Self::Message> {
         Column::new()
-        .push( Circle { radius: self.radius } )
+        .push( Circle { radius: self.value } )
+        .push( Square { half_length: self.value } )
         .push( Space::with_height(Length::Fill) )
         .push(
-            Slider::new(&mut self.slider_state, 0..=256, self.radius, Message::RadiusChanged)
+            Slider::new(&mut self.slider_state, 0..=256, self.value, Message::ValueChanged)
         )
         .into()
     }
 
     fn update(&mut self, message: Self::Message) {
         match message {
-            Message::RadiusChanged(radius) => {
-                self.radius = radius;
+            Message::ValueChanged(value) => {
+                self.value = value;
             }
         }
     }
@@ -75,19 +76,76 @@ impl<Message, B> Widget<Message, Renderer<B>> for Circle where B: Backend {
         _cursor_position: iced::Point, 
         _viewport: &iced::Rectangle
     ) -> (Primitive, mouse::Interaction) {
-        (Primitive::Quad {
-            bounds: layout.bounds(),
-            background: Background::Color(Color::BLACK),
-            border_radius: self.radius as f32,
-            border_width: 0.0,
-            border_color: Color::TRANSPARENT
-        },
-        mouse::Interaction::default()
+        (
+            Primitive::Quad {
+                bounds: layout.bounds(),
+                background: Background::Color(Color::BLACK),
+                border_radius: self.radius as f32,
+                border_width: 0.0,
+                border_color: Color::TRANSPARENT
+            },
+            mouse::Interaction::default()
         )
     }
 }
 
 impl<'a, B> Into<Element<'a, Message, Renderer<B>>> for Circle where B: Backend {
+    fn into(self) -> Element<'a, Message, Renderer<B>> {
+        Element::new(self)
+    }
+}
+
+
+pub struct Square {
+    half_length: u16
+}
+
+impl<Message, B> Widget<Message, Renderer<B>> for Square where B: Backend {
+    fn width(&self) -> Length {
+        Length::Shrink
+    }
+
+    fn height(&self) -> Length {
+        Length::Shrink
+    }
+
+    fn layout(&self, _renderer: &Renderer<B>, _limits: &layout::Limits) -> layout::Node {
+        layout::Node::new(Size::new(self.half_length as f32 * 2.0, self.half_length as f32 * 2.0))
+    }
+
+    fn hash_layout(&self, state: &mut iced_native::Hasher) {
+        use std::hash::Hash;
+
+        f32::to_bits(self.half_length as f32).hash(state)
+    }
+
+    fn draw(&self, 
+        _renderer: &mut Renderer<B>, 
+        _defaults: &Defaults, 
+        layout: iced_native::Layout<'_>, 
+        _cursor_position: iced::Point, 
+        _viewport: &iced::Rectangle
+    ) -> (Primitive, mouse::Interaction) {
+        let bounds = Rectangle {
+            width: self.half_length as f32 * 2.0,
+            height: self.half_length as f32 * 2.0,
+            ..layout.bounds()
+        };
+        
+        (
+            Primitive::Quad {
+                bounds,
+                background: Background::Color(Color::BLACK),
+                border_radius: 0.0,
+                border_width: 0.0,
+                border_color: Color::TRANSPARENT,
+            },
+            mouse::Interaction::default()
+        )
+    }
+}
+
+impl<'a, B> Into<Element<'a, Message, Renderer<B>>> for Square where B: Backend {
     fn into(self) -> Element<'a, Message, Renderer<B>> {
         Element::new(self)
     }
